27aae2827cb39841aee04b2d498cac6ec4e64573
Denys <denyskhimiy2005@gmail.com>
2025-09-16 15:24:29 +0300

    Add smooth scrolling and anchors.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
-document.getElementById("load").addEventListener("click", async () => {
-  const output = document.getElementById("output");
-  output.textContent = "Завантаження...";
-  try {
-    output.style.display = "block";
-    const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
-    const data = await res.json();
-    output.textContent = JSON.stringify(data, null, 2);
-  } catch (e) {
-    output.textContent = "Помилка завантаження: " + e.message;
-  }
+function loadData(source, buttonName, outputName) {
+  document.getElementById(buttonName).addEventListener("click", async () => {
+    const output = document.getElementById(outputName);
+    output.textContent = "Завантаження...";
+    try {
+      output.style.display = "block";
+      const res = await fetch(source);
+      const data = await res.json();
+      output.textContent = JSON.stringify(data, null, 2);
+    } catch (e) {
+      output.textContent = "Помилка завантаження: " + e.message;
+    }
+  });
+}
+
+loadData("https://jsonplaceholder.typicode.com/users/1", "loadUser", "outputUser");
+loadData("https://jsonplaceholder.typicode.com/posts/1", "loadPost", "outputPost");
+loadData("https://jsonplaceholder.typicode.com/albums/1", "loadAlbum", "outputAlbum");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        up.style.display = 'block';
+    } else {
+        up.style.display = 'none';
+    }
 });
